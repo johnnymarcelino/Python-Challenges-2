364c88816f0ef75c1222c9e87316096cace452e7
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-07-12 00:14:23 -0300

    Replaced function name "variádica" to "variadica"

--- Exerc/Exe07.py
+++ Exerc/Exe07.py
@@ -3,8 +3,8 @@
 # 7) Escreva um algoritmo capaz de receber uma quantidade variável de parâmetros.
 # Em seguida, imprima os parâmetros recebidos na tela:
 
-def variádica(*args):
+def variadica(*args):
     print(f"Os argumentos são: {args}")
 
 
-variádica(4, "johnny", 49, True, False, "Marcelino")
+variadica(4, "johnny", 49, True, False, "Marcelino")
